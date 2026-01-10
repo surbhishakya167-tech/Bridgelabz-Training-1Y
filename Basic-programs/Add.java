9e1995c0b25fb1a2b442f1bd222702f619a93c91
surbhishakya167-tech <surbhishakya167@gmail.com>
2026-01-10 21:34:27 +0530

    Added new program

---
+++ Basic-programs/Add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Add{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a,b,sum;
+		System.out.println("input 1st no");
+		a = sc.nextInt();
+		System.out.println("input 2nd no");
+		b = sc.nextInt();
+		sum = a+b;
+		System.out.println("sum="+sum);
+}}
