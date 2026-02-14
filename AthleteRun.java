89c2d58ddbb47e51a81520c875b5deff4a1d0ae2
surbhishakya167-tech <surbhishakya167@gmail.com>
2026-02-14 22:17:43 +0530

    [surbhi shakya]Add.program

---
+++ AthleteRun.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class AthleteRun {
+
+    public static double calculateRounds(double a, double b, double c) {
+        double perimeter = a + b + c;
+        return 5000 / perimeter;   // 5 km = 5000 meters
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter side 1: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter side 2: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Enter side 3: ");
+        double c = sc.nextDouble();
+
+        double rounds = calculateRounds(a, b, c);
+
+        System.out.println("Athlete must complete " + rounds + " rounds.");
+    }
+}
---
+++ SmallestLargest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class SmallestLargest{
+
+    public static int[] findSmallestAndLargest(int number1, int number2, int number3) {
+        int smallest = Math.min(number1, Math.min(number2, number3));
+        int largest = Math.max(number1, Math.max(number2, number3));
+
+        return new int[]{smallest, largest};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter three numbers: ");
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+
+        int[] result = findSmallestAndLargest(n1, n2, n3);
+
+        System.out.println("Smallest: " + result[0]);
+        System.out.println("Largest: " + result[1]);
+    }
+}
